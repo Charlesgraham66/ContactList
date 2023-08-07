066299e459998465c914e0bbf3fea38628f8322f
Charles Graham <charlesgraham@MacBook-Pro.attlocal.net>
2023-08-07 13:07:25 -0700

    finished selected contact view

--- ContactList/src/components/ContactList.jsx
+++ ContactList/src/components/ContactList.jsx
@@ -5,14 +5,14 @@ import { useState, useEffect } from "react"
 
 
 
-    // const dummyContacts = [
+    const dummyContacts = [
         
-    //     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    //     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    //     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-    //   ];
-      const ContactList = () => {
-        const [contacts, setContacts] = useState([])
+        { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+        { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+        { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+      ];
+      export default function ContactList({setSelectedContactId})  {
+        const [contacts, setContacts] = useState(dummyContacts);
         console.log(contacts)
         useEffect(() => {
             async function fetchContacts() {
@@ -43,7 +43,7 @@ import { useState, useEffect } from "react"
                     <td>Phone</td>
                 </tr>
                 {
-                    // map over data here
+                    
                     contacts.map((contact) => {
                         return <ContactRow key={contact.id} contact={contact}
                         />;
@@ -53,4 +53,5 @@ import { useState, useEffect } from "react"
     );
 }  
 
-export default ContactList
+
+
