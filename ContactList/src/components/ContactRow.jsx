c41678d6ee7f037e439051abf930cfc9dc75b8b3
Charles Graham <charlesgraham@MacBook-Pro.attlocal.net>
2023-08-08 10:45:23 -0700

    finished App

--- ContactList/src/components/ContactRow.jsx
+++ ContactList/src/components/ContactRow.jsx
@@ -4,11 +4,10 @@ import React from "react";
 
 export default function ContactRow ({ setSelectedContactId, contact }) {
     return (
-        <tr
+        <tr className="list"
         onClick={() => {
             setSelectedContactId(contact.id);
-        }}
-        >
+        }}>
             <td>{contact.name}</td>
             <td>{contact.email}</td>
             <td>{contact.phone}</td>
